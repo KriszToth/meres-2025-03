1e47cafe64fccce983a24d4af13184820f41bb4e
KriszToth <2004kiki01@gmail.com>
2025-03-21 10:17:15 +0100

    programozas

---
+++ 1_programozas/RealEstate/RealEstate/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+public class Ad
+{
+    public int Id { get; set; }
+    public int Rooms { get; set; }
+    public string LatLong { get; set; }
+    public int Floors { get; set; }
+    public int Area { get; set; }
+    public string Description { get; set; }
+    public bool FreeOfCharge { get; set; }
+    public string ImageUrl { get; set; }
+    public DateTime CreateAt { get; set; }
+    public Seller Seller { get; set; }
+    public Category Category { get; set; }
+
+    public double DistanceTo(double lat, double lon)
+    {
+        var coords = LatLong.Split(',').Select(c => double.Parse(c, CultureInfo.InvariantCulture)).ToArray();
+        double lat1 = coords[0];
+        double lon1 = coords[1];
+        return Math.Sqrt(Math.Pow(lat1 - lat, 2) + Math.Pow(lon1 - lon, 2));
+    }
+}
+
+public class Seller
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
+}
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public static class AdLoader
+{
+    public static List<Ad> LoadFromCsv(string filePath)
+    {
+        var ads = new List<Ad>();
+        var lines = File.ReadAllLines(filePath).Skip(1);
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(';');
+            var ad = new Ad
+            {
+                Id = int.Parse(parts[0]),
+                Rooms = int.Parse(parts[1]),
+                LatLong = parts[2],
+                Floors = int.Parse(parts[3]),
+                Area = int.Parse(parts[4]),
+                Description = parts[5],
+                FreeOfCharge = parts[6] == "1",
+                ImageUrl = parts[7],
+                CreateAt = DateTime.Parse(parts[8]),
+                Seller = new Seller
+                {
+                    Id = int.Parse(parts[9]),
+                    Name = parts[10],
+                    Phone = parts[11]
+                },
+                Category = new Category
+                {
+                    Id = int.Parse(parts[12]),
+                    Name = parts[13]
+                }
+            };
+            ads.Add(ad);
+        }
+
+        return ads;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var ads = AdLoader.LoadFromCsv("realestates.csv");
+
+        // 6. feladat: Földszinti ingatlanok átlagos alapterülete
+        var groundFloorAds = ads.Where(a => a.Floors == 0).ToList();
+        var averageArea = groundFloorAds.Average(a => a.Area);
+        Console.WriteLine($"1. feladat: Földszinti ingatlanok átlagos alapterülete: {averageArea:F2} m2");
+
+        // 8. feladat: Legközelebbi tehermentes ingatlan a Mesevár óvodához
+        var mesevarLat = 47.4164220114023;
+        var mesevarLon = 19.066342425796986;
+        var closestAd = ads
+            .Where(a => a.FreeOfCharge)
+            .OrderBy(a => a.DistanceTo(mesevarLat, mesevarLon))
+            .FirstOrDefault();
+
+        if (closestAd != null)
+        {
+            Console.WriteLine("2. feladat: Legközelebbi tehermentes ingatlan adatai:");
+            Console.WriteLine($"\tEladó neve: {closestAd.Seller.Name}");
+            Console.WriteLine($"\tEladó telefonja: {closestAd.Seller.Phone}");
+            Console.WriteLine($"\tAlapterület: {closestAd.Area} m2");
+            Console.WriteLine($"\tSzobák száma: {closestAd.Rooms}");
+        }
+        else
+        {
+            Console.WriteLine("8. feladat: Nincs tehermentes ingatlan a közelben.");
+        }
+    }
+}
---
+++ 1_programozas/RealEstateGUI/RealEstateGUI/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace RealEstateGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(86, 420);
+            this.listBox1.TabIndex = 0;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(12, 414);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(86, 17);
+            this.hScrollBar1.TabIndex = 1;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(81, 12);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 402);
+            this.vScrollBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Eladó neve";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Eladó telefonszáma:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(125, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(663, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Hírdetések betöltése";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(125, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Hírdetések száma:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Ingatlanok";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
